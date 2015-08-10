5242a6dd41f46b5b6c0bbcec27d5d01ac4073c70
Elena Ceseracciu <eowyn83@gmail.com>
2015-08-10 02:44:27 +0200

    Add license headers

--- test/muscleParameterComparer/muscleParameterComparer.cpp
+++ test/muscleParameterComparer/muscleParameterComparer.cpp
@@ -1,5 +1,22 @@
-// Author: Elena Ceseracciu
-// Date: Octorber 2014
+/* --------------------------------------------------------------------------*
+*                  Muscle Optimizer: muscleParameterComparer.cpp             *
+* -------------------------------------------------------------------------- *
+*                                                                            *
+* Copyright 2015  Elena Ceseracciu, Luca Modenese                            *
+*                                                                            *
+* Licensed under the Apache License, Version 2.0 (the "License");            *
+* you may not use this file except in compliance with the License.           *
+* You may obtain a copy of the License at                                    *
+*                                                                            *
+*     http://www.apache.org/licenses/LICENSE-2.0                             *
+*                                                                            *
+* Unless required by applicable law or agreed to in writing, software        *
+* distributed under the License is distributed on an "AS IS" BASIS,          *
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
+* See the License for the specific language governing permissions and        *
+* limitations under the License.                                             *
+* ---------------------------------------------------------------------------*/
+// Author: Elena Ceseracciu elena.ceseracciu@gmail.com
 
 #include <string>
 #include <iostream>
